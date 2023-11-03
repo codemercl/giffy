d98181fdad04737fd78d86f3b1a26b73b45b29e3
codemercl <developer.venium@gmail.com>
2023-11-03 17:09:27 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  // base: '/gifproject/',
+  base: '/giffy/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
